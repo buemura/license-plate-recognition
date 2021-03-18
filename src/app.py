6daaff0e3ca8ee56c86c7253ac911530b66df8e0
BrunoUemura <bruno.h.uemura@outlook.com>
2021-03-17 23:16:28 -0300

    Refactoring code

--- src/app.py
+++ src/app.py
@@ -1,27 +1,5 @@
-from lib.filters import *
-
-
-def apply_filter(plate):
-    gray = get_grayscale(plate)
-    thresh = thresholding(gray)
-    print("Altura (height): %d pixels" % (plate.shape[0]))
-    print("Largura (width): %d pixels" % (plate.shape[1]))
-    print("Canais (channels): %d" % (plate.shape[2]))
-    return thresh
-
-
-def scan_plate(image):
-    custom_config = r'-c tessedit_char_blacklist=abcdefghijklmnopqrstuvwxyz/ --psm 6'
-    value = (pytesseract.image_to_string(image, config=custom_config))
-    return value[:-2]
-
-
-def validate_plate(plate_number):
-    if plate_number == 'FUN-0972' or plate_number == 'BRA2E19':
-        print(f'{plate_number} - AUTHORIZED')
-    else:
-        print(f'{plate_number} - NOT AUTHORIZED')
-
+import cv2
+from lib.filters import get_grayscale, thresholding, pytesseract
 
 authorized_plate = ['FUN-0972', 'BRA2E19']
 
@@ -31,6 +9,29 @@ images = [
     '../images/placa3.jpg',
     '../images/placa4.jpg'
 ]
+
+
+def apply_filter(plate):
+    gray = get_grayscale(plate)
+    thresh = thresholding(gray)
+    print("Height: %d pixels" % (plate.shape[0]))
+    print("Width: %d pixels" % (plate.shape[1]))
+    print("Channels: %d" % (plate.shape[2]))
+    return thresh
+
+
+def scan_plate(image):
+    custom_config = r'-c tessedit_char_blacklist=abcdefghijklmnopqrstuvwxyz/ --psm 6'
+    plate_number = (pytesseract.image_to_string(image, config=custom_config))
+    return plate_number[:-2]
+
+
+def validate_plate(plate_number):
+    if plate_number in authorized_plate:
+        print(f'{plate_number} - AUTHORIZED')
+    else:
+        print(f'{plate_number} - NOT AUTHORIZED')
+
 
 plates = []
 plates_filter_applied = []
@@ -42,13 +43,14 @@ for i in images:
 
 # Calls the function apply_filter() passing the plate image
 for i, value in enumerate(plates):
+    print(f'\n========== Image {i + 1} details ==========')
     plates_filter_applied.append(apply_filter(plates[i]))
-    cv2.imshow('Thresh', plates_filter_applied[i])
 
+# Calls the function scan_plate() passing the plate image with filter applied
 for i, value in enumerate(plates_filter_applied):
     plates_numbers.append(scan_plate(plates_filter_applied[i]))
 
+# Calls the function validate_plate() passing the plate number
+print('\n========== Validating Plates ========== ')
 for i, value in enumerate(plates_numbers):
     validate_plate(plates_numbers[i])
-
-cv2.waitKey(0)
